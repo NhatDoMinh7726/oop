d0e36358b3d83020d2ec1b558d31a235c60f490b
24020258 Đỗ Minh Nhật <24020258@vnu.edu.vn>
2025-09-30 12:48:56 +0700

    hw1

---
+++ HW1/Introduction.java
@@ -0,0 +1,25 @@
+
+public class Introduction {
+  public static void main(String[] args) {
+    System.out.println("Đỗ Minh Nhật" + '\t' +  "24020258" + '\t' + "K69I-IT6" + '\t' + "NhatDoMinh7726" + '\t' + "24020258@vnu.edu.vn");
+    System.out.print("9 bottles of beer on the wall, 9 bottles of beer.\n"
+        + "Take one down, pass it around, \n");
+    System.out.print("8 bottles of beer on the wall, 8 bottles of beer.\n"
+        + "Take one down, pass it around, \n");
+    System.out.print("7 bottles of beer on the wall, 7 bottles of beer.\n"
+        + "Take one down, pass it around, \n");
+    System.out.print("6 bottles of beer on the wall, 6 bottles of beer.\n"
+        + "Take one down, pass it around, \n");
+    System.out.print("5 bottles of beer on the wall, 5 bottles of beer.\n"
+        + "Take one down, pass it around, \n");
+    System.out.print("4 bottles of beer on the wall, 4 bottles of beer.\n"
+        + "Take one down, pass it around, \n");
+    System.out.print("3 bottles of beer on the wall, 3 bottles of beer.\n"
+        + "Take one down, pass it around, \n");
+    System.out.print("2 bottles of beer on the wall, 2 bottles of beer.\n"
+        + "Take one down, pass it around, \n");
+    System.out.print("1 bottle of beer on the wall, 1 bottle of beer.\n"
+        + "Take one down, pass it around, \n");
+    System.out.println("No more bottles of beer on the wall.");
+  }
+}
